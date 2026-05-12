df3e2f3ac987ed74c5d0bda706e577289c1e0765
Bryan <peaveybryan03@gmail.com>
2026-05-12 16:47:32 -0400

    UserJdbcClientRepo create + tests + DataAccessException

---
+++ server/src/main/java/library/drome/data/UserRepository.java
@@ -0,0 +1,9 @@
+package library.drome.data;
+
+import library.drome.models.User;
+
+public interface UserRepository {
+    // User findByEmail(String email);
+
+    User create(User user);
+}
---
+++ server/src/test/java/library/drome/data/UserJdbcClientRepositoryTest.java
@@ -0,0 +1,39 @@
+package library.drome.data;
+
+import library.drome.models.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class UserJdbcClientRepositoryTest {
+
+    @Autowired
+    private UserJdbcClientRepository repository;
+
+    @Autowired
+    private JdbcClient jdbcClient;
+
+    @BeforeEach
+    void setup() {
+        jdbcClient.sql("call set_known_good_state();").update();
+    }
+
+    @Test
+    void shouldCreate() {
+        User toCreate = TestDataHelper.userToCreate();
+        User expected = TestDataHelper.userAfterCreate();
+
+        // assertNull(repository.findByEmail(toCreate.getEmail()));
+
+        User actual = repository.create(toCreate);
+
+        assertEquals(expected, actual);
+        // assertNotNull(repository.findByEmail(toCreate.getEmail()));
+    }
+
+}

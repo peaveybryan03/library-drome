fbfcdee3a9b4fbece1f145be64ec9a8c4116b5fe
Bryan <peaveybryan03@gmail.com>
2026-05-15 10:25:48 -0400

    ListController findByUserId is authorized + http

--- server/src/main/java/library/drome/controllers/ListController.java
+++ server/src/main/java/library/drome/controllers/ListController.java
@@ -1,10 +1,14 @@
 package library.drome.controllers;
 
+import library.drome.data.DataAccessException;
 import library.drome.domain.ListService;
 import library.drome.models.FilmList;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/list")
@@ -17,9 +21,15 @@ public class ListController {
     }
 
     @GetMapping("/{userId}")
-    public List<FilmList> findByUserId(@PathVariable int userId) {
-        // should be authorized
-        return service.findByUserId(userId);
+    public ResponseEntity<Object> findByUserId(@PathVariable int userId, @RequestHeader Map<String, String> headers) throws DataAccessException {
+        AuthorizationResult authorizationResult = AuthorizationHelper.getUserFromHeaders(headers);
+
+        if (!authorizationResult.isSuccess()) {
+            return authorizationResult.getResponseEntity();
+        }
+
+        List<FilmList> result = service.findByUserId(userId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
